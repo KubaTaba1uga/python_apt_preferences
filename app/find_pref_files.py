5916fba4c439dfc2791039d0e5c2d725921e9ccd
taba1uga <taba1uga@desktop>
2023-04-19 08:38:09 +0200

    add unit-tests

--- app/find_pref_files.py
+++ app/find_pref_files.py
@@ -3,6 +3,7 @@
    Find paths to all preference files.
 
 """
+import string
 import typing
 from pathlib import Path
 
@@ -13,9 +14,11 @@ _APT_PREF_DIR_PATH_S: str = _APT_PREF_FILE_PATH_S + ".d"
 
 
 def find_pref_files() -> typing.List[Path]:
-    pref_files_paths: typing.List[Path] = [get_default_pref_file_path()]
+    """ Find paths to files used by APT to set up preferences. """
 
-    for file_name in get_default_pref_dir_path().rglob("*"):
+    pref_files_paths: typing.List[Path] = [_get_default_pref_file_path()]
+
+    for file_name in _get_default_pref_dir_path().rglob("*"):
         file_path = Path(file_name)
 
         if is_pref_file(file_path) is True:
@@ -24,18 +27,36 @@ def find_pref_files() -> typing.List[Path]:
     return pref_files_paths
 
 
-def get_default_pref_file_path() -> Path:
+def _get_default_pref_file_path() -> Path:
     """ The APT preferences file '/etc/apt/preferences'. """
     return Path(_APT_PREF_FILE_PATH_S)
 
 
-def get_default_pref_dir_path() -> Path:
+def _get_default_pref_dir_path() -> Path:
     """ The APT preferences fragment files in the '/etc/apt/preferences.d/'."""
     return Path(_APT_PREF_DIR_PATH_S)
 
 
 def is_pref_file(path: Path) -> bool:
-    """The files have either no or "pref" as filename extension and only
-    contain alphanumeric, hyphen (-), underscore (_) and period (.) characters.
+    """According docs:
+    `The files have either no or "pref" as filename extension and only
+    contain alphanumeric, hyphen (-), underscore (_) and period (.) characters.`
     """
+    allowed_extensions, allowed_characters = ["", ".pref"], [
+        *string.ascii_letters,
+        *string.digits,
+        "-",
+        "_",
+        ".",
+    ]
+
+    file_extension, file_content = path.suffix, path.read_text()
+
+    if file_extension not in allowed_extensions:
+        return False
+
+    for character in file_content:
+        if character not in allowed_characters:
+            return False
+
     return True
---
+++ test/test_find_pref_files.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from unittest import mock
+
+from app.find_pref_files import is_pref_file
+
+
+def test_is_pref_file_pref_extension(tmpdir):
+    wrong_extension, correct_extension, no_extension = (
+        Path(tmpdir.join("hello.txt")),
+        Path(tmpdir.join("hello.pref")),
+        Path(tmpdir.join("hello")),
+    )
+
+    with mock.patch.object(Path, "read_text") as read_text_mock:
+        read_text_mock.return_value = ""
+
+        assert is_pref_file(wrong_extension) is False
+        assert is_pref_file(correct_extension) is True
+        assert is_pref_file(no_extension) is True
+
+
+def test_is_pref_file_allowed_characters(tmpdir):
+    wrong_characters, correct_characters, no_characters = (
+        tmpdir.join("invalid_characters.pref"),
+        tmpdir.join("correct_characters.pref"),
+        tmpdir.join("no_characters.pref"),
+    )
+
+    wrong_characters.write_text("!@#$%^&*()", encoding="utf-8")
+    correct_characters.write_text("iuahsduhaduhad", encoding="utf-8")
+
+    wrong_characters, correct_characters, no_characters = (
+        Path(wrong_characters),
+        Path(correct_characters),
+        Path(no_characters),
+    )
+
+    assert is_pref_file(wrong_characters) is False
+    assert is_pref_file(correct_characters) is True
+    assert is_pref_file(no_characters) is True
